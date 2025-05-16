c9affa35a5af57507da38617ce8848811a7ee2b3
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2025-05-17 03:46:39 +0800

    ci: fix permission issues

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 URL="https://techassessment.blob.core.windows.net/aiip6-assessment-data/bmarket.db"
 DEST_DIR="/data"
-mkdir -p "$DEST_DIR"
-curl -o "$DEST_DIR/bmarket.db" "$URL"
-python src/main.py
+sudo mkdir -p "$DEST_DIR"
+sudo curl -o "$DEST_DIR/bmarket.db" "$URL"
+sudo python src/main.py
