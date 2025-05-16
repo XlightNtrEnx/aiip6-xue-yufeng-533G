c28bddc0ef98bd3deb904726e86175742c6b83a0
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2025-05-17 03:56:44 +0800

    fix(run.sh): downloading to /data instead of ./data

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL="https://techassessment.blob.core.windows.net/aiip6-assessment-data/bmarket.db"
-DEST_DIR="/data"
+DEST_DIR="data"
 sudo mkdir -p "$DEST_DIR"
 sudo curl -o "$DEST_DIR/bmarket.db" "$URL"
 python src/main.py
